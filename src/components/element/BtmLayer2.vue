a15df910a7049c4978d015d7c4e2419edfc3f44e
seoeunan <seoeunan@naver.com>
2022-03-08 10:56:41 +0900

    test

---
+++ src/components/element/BtmLayer2.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <template v-if="content">
+            <slot ref="content" name="content" />
+        </template>
+    </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+    props:{
+        content: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
